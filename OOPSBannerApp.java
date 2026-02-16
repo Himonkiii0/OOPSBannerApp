26ed8c88b5f48178e66fa60b2aaedf57b655b03e
Himonkiii0 <hb3994@srmist.edu.in>
2026-02-16 13:20:31 +0530

    Refracted UC2 to use String.join() for memory efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,19 @@
 /**
- * OOPSBannerApp UC2 Banner Display Application
+ * OOPSBannerApp UC3 OOPS Banner Application
+ * This class refactors UC2 to use String.join() for better memory efficiency
+ * and readability, avoiding the overhead of multiple intermediate String objects.
  * @author [Your Name]
- * @version 2.0
+ * @version 3.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        // Line 1
-        System.out.println("  *****  " + "  *****  " + " ******* " + "  ***** ");
-        // Line 2
-        System.out.println(" *     * " + " *     * " + " *     * " + " *     * ");
-        // Line 3
-        System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
-        // Line 4
-        System.out.println(" *     * " + " *     * " + " ******* " + "  *****  ");
-        // Line 5
-        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
-        // Line 6
-        System.out.println(" *     * " + " *     * " + " *       " + " *     * ");
-        // Line 7
-        System.out.println("  *****  " + "  *****  " + " *       " + "  *****  ");
+        // Construct each line using String.join with an empty delimiter ""
+        System.out.println(String.join("", "  *****  ", "  *****  ", " ******* ", "  ***** "));
+        System.out.println(String.join("", " *     * ", " *     * ", " *     * ", " *     * "));
+        System.out.println(String.join("", " *     * ", " *     * ", " *     * ", " *       "));
+        System.out.println(String.join("", " *     * ", " *     * ", " ******* ", "  *****  "));
+        System.out.println(String.join("", " *     * ", " *     * ", " *       ", "       * "));
+        System.out.println(String.join("", " *     * ", " *     * ", " *       ", " *     * "));
+        System.out.println(String.join("", "  *****  ", "  *****  ", " *       ", "  *****  "));
     }
 }
